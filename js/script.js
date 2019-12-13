acfa190fa2355a132472b4ccfbcae59ab9334237
Radoslaw Zaborski <zaborski.radoslaw@gmail.com>
2019-12-13 15:03:28 +0100

    correct select active article function in script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ for(let activeLink of activeLinks){
   
   /* [done] remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.posts a.active');
+  const activeArticles = document.querySelectorAll( ' .posts .active');
   
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
